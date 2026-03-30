bb45d07f59bdbc9688e87465683b66fa799905b5
skhan2003 <sadia_kh80@yahoo.com>
2026-03-30 02:48:36 +0100

    Update Telegram bot credentials and add keep-alive
    Updated the TOKEN and CHAT_ID for the Telegram bot and added a keep-alive loop.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
+import time
 
-TOKEN = "YOUR_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+TOKEN = "8522684488:AAGBLIhapSR42j6HAWAcNHl-ipqJN8eU828"
+CHAT_ID = "7216850185"
 
 def send(msg):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
 
+# start message
 send("🚀 BOT IS LIVE")
+
+# keep app alive (IMPORTANT)
+while True:
+    time.sleep(60)
