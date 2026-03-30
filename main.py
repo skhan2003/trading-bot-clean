f93e1a98ae9ab318c480d790d8248642dd0c9707
skhan2003 <sadia_kh80@yahoo.com>
2026-03-30 01:54:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ import time
 TOKEN = "8522684488:AAGBLIhapSR42j6HAWAcNHl-ipqJN8eU828"
 CHAT_ID = "7216850185"
 
-API_URL = "https://financialmodelingprep.com/api/v3/stock_market/gainers?apikey=wg6hAv7crwZdlFQcmoYwKdYqnK0cXaXD"
-
 def send_message(text):
     url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
     requests.post(url, data={"chat_id": CHAT_ID, "text": text})
@@ -14,16 +12,19 @@ while True:
     send_message("📡 Scanning market...")
 
     try:
-        data = requests.get(API_URL).json()
+        # FREE WORKING API
+        url = "https://query1.finance.yahoo.com/v7/finance/screener/predefined/saved?count=10&scrIds=day_gainers"
+        data = requests.get(url).json()
 
-        if isinstance(data, list):
-            for stock in data[:5]:
-                if stock.get("price", 0) < 5:
-                    send_message(
-                        f"🚀 {stock['symbol']} | ${stock['price']} | {stock['changesPercentage']}"
-                    )
-        else:
-            send_message("⚠️ API error")
+        stocks = data["finance"]["result"][0]["quotes"]
+
+        for stock in stocks:
+            price = stock.get("regularMarketPrice", 0)
+
+            if price and price < 5:
+                send_message(
+                    f"🚀 {stock['symbol']} | ${price}"
+                )
 
     except Exception as e:
         send_message(f"Error: {e}")
